eee02452721b71b7d14939670ed4da0a768f3f3b
Kasper Flye <kasperflye@gmail.com>
2026-01-27 21:46:45 +0100

    change gaming layer to qwerty

--- keyboards/keychron/v1/iso_encoder/keymaps/kflye/keymap.c
+++ keyboards/keychron/v1/iso_encoder/keymaps/kflye/keymap.c
@@ -29,10 +29,10 @@
      N300, N301, N302, K303, K304, K305,                K306, K307, K308, N309, N310, K311\
 ) \
 LAYOUT_keychron_v1_wrapper(\
-    KC_ESC,     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  QK_BOOT,     KC_MPLY,\
+    KC_ESC,     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,      KC_MPLY,\
     KC_GRV,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,     KC_PGUP,\
-    K000,       K001,    K002,    K003,    K004,    K005,    K006,    K007,    K008,    K009,    K010,    KC_LBRC, KC_RBRC,              KC_HOME,\
-    K100,       K101,    K102,    K103,    K104,    K105,    K106,    K107,    K108,    K109,    K110,    KC_NUHS, KC_RSFT, KC_ENT,      KC_END,\
+    K000,       K001,    K002,    K003,    K004,    K005,    K006,    K007,    K008,    K009,    K010,    KC_LBRC, KC_RBRC,              KC_PGDN,\
+    K100,       K101,    K102,    K103,    K104,    K105,    K106,    K107,    K108,    K109,    K110,    KC_NUHS, KC_RSFT, KC_ENT,      KC_HOME,\
     K200,       KC_NUBS, K201,    K202,    K203,    K204,    K205,    K206,    K207,    K208,    K209,    K210,    KC_RSFT,     KC_UP,          \
     KC_LCTL,    K303,    K304,                K305,                                            K306,   K307,   K308,   KC_LEFT, KC_DOWN, KC_RGHT\
 )
@@ -59,11 +59,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _________________THUMB_LEFT_________________, _________________THUMB_RIGHT________________
     ),
     [_GAMING] = LAYOUT_base_wrapper(
-        _________________GAMING_L1__________________, _________________GAMING_R1__________________,
-        _________________GAMING_L2__________________, _________________GAMING_R2__________________,
-        _________________GAMING_L3__________________, _________________GAMING_R3__________________,
-        _________________GAMING_L4__________________, _________________GAMING_R4__________________
+        ______________GAMING_QWERTY_L1______________, ______________GAMING_QWERTY_R1______________,
+        ______________GAMING_QWERTY_L2______________, ______________GAMING_QWERTY_R2______________,
+        ______________GAMING_QWERTY_L3______________, ______________GAMING_QWERTY_R3______________,
+        ______________GAMING_QWERTY_L4______________, ______________GAMING_QWERTY_R4______________
     ),
+    // [_GAMING] = LAYOUT_base_wrapper(
+    //     _________________GAMING_L1__________________, _________________GAMING_R1__________________,
+    //     _________________GAMING_L2__________________, _________________GAMING_R2__________________,
+    //     _________________GAMING_L3__________________, _________________GAMING_R3__________________,
+    //     _________________GAMING_L4__________________, _________________GAMING_R4__________________
+    // ),
     [_SYM] = LAYOUT_base_wrapper(
         ___________________SYM_L1___________________, ___________________SYM_R1___________________,
         ___________________SYM_L2___________________, ___________________SYM_R2___________________,
